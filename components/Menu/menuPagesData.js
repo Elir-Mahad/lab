13d1c2ec91c7a24876b36e02760a2331f887f19a
elir-mahad <mmahad280@gmail.com>
2023-12-15 07:26:35 -0500

    enhance(menu): add correct menu links

--- components/Menu/menuPagesData.js
+++ components/Menu/menuPagesData.js
@@ -1,18 +1,28 @@
 //
 export const allPortfolioPages = [
   {
-    id: "home-page",
-    href: "/",
+    id: "home",
+    href: "https://mahad-lab.vercel.app/",
     pageName: "Home",
   },
   {
     id: "projects",
-    href: "/projects",
+    href: "https://mahad-lab.vercel.app/projects",
     pageName: "Projects",
   },
   {
+    id: "notes",
+    href: "https://mahad-notes.vercel.app/",
+    pageName: "Study Notes",
+  },
+  {
+    id: "random-notes",
+    href: "https://mahad-notes.vercel.app/random-notes",
+    pageName: "Random notes",
+  },
+  {
     id: "contact",
-    href: "/contact",
+    href: "https://mahad-lab.vercel.app/contact",
     pageName: "Contact",
   },
 ];
